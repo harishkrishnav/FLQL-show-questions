85fa0138642a588121140ddc220ee34da42430a8
Harish Krishna <hakr@microsoft.com>
2022-04-20 16:54:29 -0700

    Better file on-load message
    Now prints the name of the file on screen

--- popup.js
+++ popup.js
@@ -2,46 +2,47 @@ let loadButton = document.getElementById("loadButton");
 
 loadButton.addEventListener("click", async () => {
   var fileToLoad = document.getElementById("fileToLoad").files[0];
+  var fileName = document.getElementById("fileToLoad").files[0].name;
   var fileReader = new FileReader();
   fileReader.onload = async function (fileLoadedEvent) {
     var textFromFileLoaded = fileToArray(fileLoadedEvent.target.result);
     chrome.storage.local.set({ questions: textFromFileLoaded });
-    console.log(textFromFileLoaded);
+    //console.log(textFromFileLoaded);
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: displayFileLoaded,
+      args: [textFromFileLoaded, fileName]
     });
   };
 
   fileReader.readAsText(fileToLoad, "UTF-8");
 });
 
-function displayFileLoaded() {
+async function displayFileLoaded(questions, fileToLoad) {
+  //alert("A file has been loaded. You are ready to begin the quiz.")
   var element = document.getElementById("instructions");
-  if (element) {
-    var numberOfQuestions = 0;
-    chrome.storage.sync.get("questions", ({ questions }) => {
-      numberOfQuestions = questions.length;
-      if (numberOfQuestions > 0) {
-        try {
-          const loaded = document.createElement("p");
-          loaded.className = "loaded";
-          loaded.innerText = "Loaded questions";
-          loaded.style.marginTop = "-10px";
-          element.append(loaded);
-        } catch (e) {}
-      }
-    });
+  if (element && questions) {
+    if (questions.length > 0) {
+      try {
+        const loaded = document.createElement("p");
+        loaded.className = "loaded";
+        loaded.innerText = "Loaded questions from "+fileToLoad;
+        element.append(loaded);
+      } catch (e) {}
+    }
   }
 }
 
 //copied from StackOverflow - modify based on actual FLQL format
 function fileToArray(str, delimiter = "\t") {
+
   // slice from start of text to the first \n index
   // use split to create an array from string by delimiter
-  const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
-
+  var headers = str.slice(0, str.indexOf("\n")).split(delimiter);
+  headers = headers.map((h) => h.trim());
+  console.log(headers);
+  
   // slice from \n index + 1 to the end of the text
   // use split to create an array of each csv value row
   const rows = str.slice(str.indexOf("\n") + 1).split("\n");
@@ -60,6 +61,5 @@ function fileToArray(str, delimiter = "\t") {
     return el;
   });
 
-  // return the array
   return arr;
 }
